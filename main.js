c4484eb06bb14700e8cbb9e01c12631d6797f39a
Gustavo Maciel <gustamaciel2012@hotmail.com>
2020-06-07 01:05:36 -0300

    Added Axios library

--- main.js
+++ main.js
@@ -1,23 +1,5 @@
-var myPromise = function () {
-  return new Promise(function (resolve, reject) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.open("GET", "https://api.github.com/users/gustavottc");
-    xhr.send(null);
-
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          resolve(JSON.parse(xhr.responseText));
-        } else {
-          reject("Error on request");
-        }
-      }
-    };
-  });
-};
-
-myPromise()
+axios
+  .get("http://api.github.com/users/gustavottc")
   .then(function (response) {
     console.log(response);
   })
